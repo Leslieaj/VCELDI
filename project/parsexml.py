15bca875ba78e423246c6192dbbc0fa057566cae
leslie <ajstar1990@sina.com>
2016-12-20 20:59:04 +0800

    add to class Location  a new  attribute 'next' which means the transitions out of the location. It marks the transitions have been visited to help find the path

--- project/parsexml.py
+++ project/parsexml.py
@@ -25,14 +25,20 @@ class Label:
 		self.value = value
 
 class Location:
-	def __init__(self, id, name=None, invariant=None, init=False):
+	def __init__(self, id, name=None, invariant=None, init=False, transitions=[]):
 		self.id = id
 		self.name = name
 		self.invariant = invariant
 		self.init = init
-		
+		self.next = self.initNexttransition(transitions) or {}
+	def initNexttransition(self, transitions):
+		nexttr = {}
+		for tr in transitions:
+			if tr.source == self.id:
+				nexttr[tr.id] = False
+		return nexttr
 	def getlocation(self):
-		return [self.id, self.name, self.invariant, self.init]
+		return [self.id, self.name, self.invariant, self.init, self.next]
 
 
 class Transition:
@@ -47,17 +53,17 @@ class Transition:
 	def gettransition(self):
 		return [self.id, self.source, self.target, self.guard, self.assignment]
 		
-class Nexttransitions:
+"""class Nexttransitions:
 	def __init__(self, locationid, template):
 		self.location = locationid
-		self.next = self.initNexttransition(template) or []
+		self.next = self.initNexttransition(template) or {}
 
 	def initNexttransition(self, template):
 		nexttr = {}
 		for tr in template.transitions:
 			if tr.source == self.location:
-					nexttr[tr.id] = False
-		return nexttr
+				nexttr[tr.id] = False
+		return nexttr"""
 
 def init(xmlfile):
 	tree = ET.parse(xmlfile)
@@ -71,14 +77,6 @@ def parseXML(root):
 	transitionid = 0
 	for templatexml in root.getiterator("template"):
 		declaration = templatexml.findtext("declaration") or ""
-		locations = []
-		for locationxml in templatexml.getiterator("location"):
-			name = locationxml.findtext("name")
-			location = Location(id=locationxml.get('id'),name=name)
-			for labelxml in locationxml.getiterator("label"):
-				if labelxml.get('kind') == 'invariant':
-					location.invariant = labelxml.text
-			locations += [location]
 		transitions = []
 		for transitionxml in templatexml.getiterator("transition"):
 			sourceL = transitionxml.find('source').get('ref')
@@ -94,6 +92,14 @@ def parseXML(root):
 				if labelxml.get('kind') == 'assignment':
 					transition.assignment = labelxml.text
 			transitions += [transition]
+		locations = []
+		for locationxml in templatexml.getiterator("location"):
+			name = locationxml.findtext("name")
+			location = Location(id=locationxml.get('id'),name=name, transitions=transitions)
+			for labelxml in locationxml.getiterator("label"):
+				if labelxml.get('kind') == 'invariant':
+					location.invariant = labelxml.text
+			locations += [location]
 		if templatexml.find("init") != None:
 			initlocation = templatexml.find("init").get('ref')
 			for l in locations:
@@ -108,17 +114,17 @@ def parseXML(root):
 def main():
 	ntaxml = init(sys.argv[1])
 	templates = parseXML(ntaxml)
-	next = []
+	#next = []
 	for t in iter(templates):
 		print t.name, t.declaration
 		for l in t.locations:
-			next+=[Nexttransitions(l.id, t)]
+			#next+=[Nexttransitions(l.id, t)]
 			print l.getlocation()
 		for tr in t.transitions:
 			print tr.gettransition()
 
-	for i in next:
-		print i.location, i.next.items()
+	#for i in next:
+		#print i.location, i.next
 
 if __name__=='__main__':
 	main()
--- project/path.py
+++ project/path.py
@@ -9,36 +9,84 @@ class Observationinterval:
 	def getub(self):
 		return self.ub
 	def getOInterval(self):
-		return '[' + str(self.lb) + ',' + str(self.ub) + ']'
+		return 't>=' + str(self.lb) + '&&' + 't<=' + str(self.ub) 
 
 class PLocation:
-	def __init__(self, locationid, index, federation)
-		self.id = locationid
+	def __init__(self, location, index, federation):
+		self.id = location.id
 		self.index = index
+		self.invariant = location.invariant
 		self.federation = federation
-	def getLocationVariable()
+		self.next = self.initnext(location) or {}
+	def initnext(self, location):
+		nexttr = {}
+		#print location.next
+		for key, value in location.next.items():
+			nexttr[key] = value
+		return nexttr
+	def getLocationVariable(self):
 		return 't_'+ str(self.index)
-
-
+	#if there is an out transition has not been selected, return the tansition id, 
+	#	else, means all out transitions of the location have selected, return False.
+	def hasnexttran(self):
+		for tranid, hasselected in next:
+			if hasselected == False :
+				return tranid
+		return False
+	def selecttran(self,tranid):
+		if tranid in next.keys():
+			next[tranid] = True
+	def getplocation(self):
+		print self.id,self.index, self.invariant, self.federation, self.next
+		
 class Potentialpath:
-	def __init__(self, initzone)
+	def __init__(self, initzone):
 		self.initzone = initzone
 		self.path = []
-	def addPlocation(self, plocation)
+	def addPlocation(self, plocation):
 		self.path.append(plocation)
-	
 
-def findpath(zone, observationinterval):
+def findlocationbyid(id, locations):
+	for l in locations:
+		if id == l.id:
+			#print l.next
+			return l
+	return None
+
+def ointervalfed(context, ointerval):
+	interval = ointerval.getOInterval()
+	return guardtofed(context, interval)
+
+def findpath(zone, ointerval, template):
 	initzone = zone
 	paths = []
-	
-	
-
+	ppath = Potentialpath(initzone)
+	location = findlocationbyid(initzone.location, template.locations)
+	plocation = PLocation(location, len(ppath.path), initzone.federation)
+	#plocation.getplocation()
+	if plocation.invariant == None:
+		tempfed = plocation.federation.up() & ointerval
+		if not tempfed.isEmpty():
+			plocation.federation = plocation.federation.up()
+			plocation.index = plocation.index + 1
+			ppath.addPlocation(plocation)
+	ppath.path[0].getplocation()
 
 
 def main():
-	ointerval = Observationinterval(3,3)
-	print ointerval.getOInterval()
-
+	#start = time.clock()
+	ntaxml = init(sys.argv[1])
+	templates = parseXML(ntaxml)
+	v = Context(['x', 'y', 't'], 'v')
+	ceil={v.x:10,v.y:20, v.t:100}
+	bgf = v.getZeroFederation()
+	beginzone =  Zone('id2', bgf)
+	zones,enter = getzones(v,beginzone,templates[0],ceil)
+	#end = time.clock()
+	#print end-start
+	observationinterval = Observationinterval(3,3)
+	ointerval = ointervalfed(v,observationinterval)
+	findpath(enter[0], ointerval,templates[0])
+	
 if __name__=='__main__':
 	main()
